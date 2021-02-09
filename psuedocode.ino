02e428ad274014d703e79b18bdf24fb84b09fba9
Helmstk1 <54641488+Helmstk1@users.noreply.github.com>
2021-02-09 13:12:10 -0500

    Create psuedocode.ino

---
+++ psuedocode.ino
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+void setup()
+{
+// attach servo and button to their pins  
+}
+
+void loop()
+{
+  //read what the button is doing
+
+  //IF the button is pressed the first time (toggle), THEN start vibrating the servo
+
+        // Jah will write HERE, how does the servo vibrate?
+            //tell the servo to go to angle X
+            //wait a tiny amount of time
+            //tell the servo to go to angle Y
+            //wait a tiny amount of time
+
+  //IF the button is pressed the second time (toggle), THEN stop vibrating the servo 
+
+  //MrH wants me to serial print the button values and servo values
+
+
+
+}
+
