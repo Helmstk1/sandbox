0599d6042c3f851d8ccb70dbb80378fd83e8108d
Helmstk1 <54641488+Helmstk1@users.noreply.github.com>
2022-03-23 14:17:35 -0400

    edditing code, adding up to PID

--- PID/main.py
+++ PID/main.py
@@ -1,9 +1,13 @@
-# Write your code here :-)
+# PID w/ rotary encoder, based off Arduino example from Dr. Shields
+
+''' OK, so the encoder would be cool, but it makes so much extra work!!'''
+
 import board
+import time
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 import rotaryio
-
+# EEPROM?  timerone.h?  Clickencoder.h?
 
 motorPin = 3
 interruptPin = 2
@@ -57,3 +61,132 @@ encoder = rotaryio.IncrementalEncoder(board.D10, board.D9)
 button = digitalio.DigitalInOut(RotaryButton)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
+
+''' Some Timer1 stuff that may be related to pwm or rotary encoder
+	Timer1.initialize(1000);
+  	Timer1.attachInterrupt(timerIsr); 
+  	last = -1;
+'''
+
+'''LCD is already setup, no init needed
+	lcd.begin();
+	lcd.backlight();
+	lcd.createChar(0, fullArrow);
+	lcd.createChar(1, emptyArrow);
+	lcd.createChar(2, smallArrow);
+	lcd.home();
+	lcd.print(" PID:OFF ");
+	lcd.print("S:    0");
+	lcd.setCursor(0,1);
+	lcd.print(" kP .");
+	lcd.print(fixSpeed(PID[0],2,"0"));
+	lcd.print(" A:    0");
+'''
+
+''' void moveCursor() {
+	for(int i=0; i<4; i++) {
+		lcd.setCursor(cols[i],rows[i]);
+		if(i==cursorPos) 
+			lcd.write(arrowSize);
+		else
+			lcd.print(" ");
+	}
+}
+'''
+while True: 
+	now = time.monotonic()
+	if now > (lastRPM + msDelay):
+		calcRPM()
+		setMotor()
+		lastRPM = time.monotonic()
+	
+	value = value + encoderValue #rewrite with correct encoder code?
+
+	if value is not last:
+		if value > last:
+			encoderBump = 1
+		else:
+			encoderBump = -1
+		if arrowSize is 2: 		# you're moving the cursor
+			cursorPos += encoderBump
+			if cursorPos > 3
+				cursorPos = 0
+			if cursorPos < 0
+				cursorPos = 4
+		else:   				# You're changing a value
+			if cursorPos is 0:
+				lcd.setCursor(6,0)
+				pidToggle = !pidToggle
+				if pidToggle is True:
+					lcd.print("N ")
+				else:
+					lcd.print("FF")
+			elif cursorPos is 1: # You're cycling through Kp,Ki,Kd
+				lcd.setCursor(2,0)
+				pidIndex += encoderBump
+				if pidIndex > 2:
+					pidIndex = 0
+				if pidIndex < 0:
+					pidIndex = 2
+				lcd.print(pidText[pidIndex])
+				lcd.print(" .")
+				lcd.print(fixSpeed(PID[pidIndex],2,"0"))
+			elif cursorPos is 3: # ?
+				lcd.setCursor(11, 0)
+				if (setPoint < maxSpeed):
+					setPoint += encoderBump
+				setPointAct = map(setpoint,0,255,0,maxSpeed)
+				lcd.print(fixSpeed(setPointAct,4," "))
+		rotCount++
+	Clickencoder::button b = encoder->getButton()
+	if (b is not ClickEncoder::Open):
+		if b = (ClickEncoder::Clicked:):
+			if(arrowSize == 0):
+					arrowSize = 2
+				else:
+					arrowSize = 0
+		if b is (ClickEncoder::Held:):
+			print("Saving")
+			for i in range(3):
+				EEPROM.write(i,PID[i])
+			saved = True
+			# He cycled the lcd backlight, is that a thing? Dhalbert
+		if b is (ClickEncoder::Released:):
+			saved = false
+	moveCursor()
+	last = value
+
+def fixSpeed(speed, max, what):
+	str = ""
+	for i in range(log10(speed),max):
+		str += what
+	str += String(speed)
+	return str
+
+def calcRPM():
+	# detach the interupt:
+	# detachInterrupt(digitalPinToInterrupt(interruptPin));
+	RPM = count*multiplier
+	lcd.setCursor(11,1)
+	lcd.print(fixSpeed(RPM,4," "))
+	lcd.print("    ")
+	count = 0
+	# Reattach the interupt function
+	# attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+
+def setMotor():
+	if pidToggle:
+		error = setPointAct - RPM
+		P = error * PID[0]/1000
+		I = I + error * PID[1]/1000
+		D = (error-lastError)/(msDelay/1000) * PID[2]/1000
+		drive = P + I + D
+
+
+
+
+
+
+				
+
+
